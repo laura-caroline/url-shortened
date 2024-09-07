730911ec45829965ecb1c6c44c5826ef0f585021
laura <carolinelaura69@gmail.com>
2024-09-07 15:59:22 -0300

    correcao eslint

--- src/utils/handlerError/index.ts
+++ src/utils/handlerError/index.ts
@@ -2,6 +2,7 @@ import { UnauthorizedError } from 'src/modules/auth/errors/unauthorized.error';
 import { Response } from 'express';
 import { HttpStatus, NotFoundException } from '@nestjs/common';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const handleError = (response: Response, error: any) => {
   if (error instanceof UnauthorizedError) {
     return response.status(HttpStatus.UNAUTHORIZED).json({
